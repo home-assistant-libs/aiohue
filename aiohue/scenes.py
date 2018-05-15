845e09fe3ef6402d08e13602829374d037e86ecd
MizterB <accounts@chrisbarrett.com>
2018-05-15 08:31:55 -0400

    Exposing all available properties of the Scenes object

--- aiohue/scenes.py
+++ aiohue/scenes.py
@@ -21,3 +21,37 @@ class Scene:
     @property
     def name(self):
         return self.raw['name']
+
+    @property
+    def lights(self):
+        return self.raw['lights']
+
+    @property
+    def owner(self):
+        return self.raw['owner']
+
+    @property
+    def recycle(self):
+        return self.raw['recycle']
+
+    @property
+    def locked(self):
+        return self.raw['locked']
+
+    @property
+    def appdata(self):
+        return self.raw['appdata']
+
+    @property
+    def picture(self):
+        return self.raw['picture']
+
+    @property
+    def lastupdated(self):
+        return self.raw['lastupdated']
+
+    @property
+    def version(self):
+        return self.raw['version']
+
+
