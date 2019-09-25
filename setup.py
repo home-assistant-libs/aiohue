d3a03ab83cc2ded5ddbbd770a48bd5bcdb4e031a
Paulus Schoutsen <balloob@gmail.com>
2019-09-24 21:10:20 -0700

    Version bump to 1.9.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md').read()
 
 setup(
     name='aiohue',
-    version='1.9.1',
+    version='1.9.2',
     license='Apache License 2.0',
     url='https://github.com/balloob/aiohue',
     author='Paulus Schoutsen',
