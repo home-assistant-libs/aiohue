9eb24b5b682eb887a20ebd9ca64705469f11bc7c
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2021-09-14 14:20:31 -0700

    2.6.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = open('README.md').read()
 
 setup(
     name='aiohue',
-    version='2.6.1',
+    version='2.6.2',
     license='Apache License 2.0',
     url='https://github.com/home-assistant-libs/aiohue',
     author='Paulus Schoutsen',
