e02032b59fdce5b4c1211fc96cb6bf7ef521f97f
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2021-11-12 11:01:01 -0800

    3.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ MIN_PY_VERSION = "3.8"
 
 setup(
     name="aiohue",
-    version="2.7.9",
+    version="3.0.0",
     license="Apache License 2.0",
     url="https://github.com/home-assistant-libs/aiohue",
     author="Paulus Schoutsen",
