56abb1fa1d42e49037c63eeb4f5e2be54b6647ad
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-12-29 11:17:14 +0100

    3.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ MIN_PY_VERSION = "3.8"
 
 setup(
     name="aiohue",
-    version="3.0.10",
+    version="3.0.11",
     license="Apache License 2.0",
     url="https://github.com/home-assistant-libs/aiohue",
     author="Paulus Schoutsen",
