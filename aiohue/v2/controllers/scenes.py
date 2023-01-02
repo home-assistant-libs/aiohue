c3cdc1dd61ad7c54a9b0905f7e62007995109c0a
Marcel van der Veldt <m.vanderveldt@outlook.com>
2023-01-02 16:14:29 +0100

    Add support for smart_scene resource (#170)
    * Add support for smart_scene resource
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- aiohue/v2/controllers/scenes.py
+++ aiohue/v2/controllers/scenes.py
@@ -1,15 +1,26 @@
 """Controller holding and managing HUE resources of type `scene`."""
-from typing import Optional, Type, Union
+from typing import TYPE_CHECKING, Any, Optional, Type, Union
 
 from ..models.feature import DimmingFeaturePut, RecallAction, RecallFeature
 from ..models.resource import ResourceTypes
 from ..models.room import Room
 from ..models.scene import Scene, ScenePut
+from ..models.smart_scene import (
+    SmartScene,
+    SmartScenePut,
+    SmartSceneRecall,
+    SmartSceneRecallAction,
+)
 from ..models.zone import Zone
-from .base import BaseResourcesController
+from .base import BaseResourcesController, GroupedControllerBase
+
+if TYPE_CHECKING:
+    from .. import HueBridgeV2
+
+SCENE_TYPES = Union[Scene, SmartScene]
 
 
-class ScenesController(BaseResourcesController[Type[Scene]]):
+class RegularScenesController(BaseResourcesController[Type[Scene]]):
     """Controller holding and managing HUE resources of type `scene`."""
 
     item_type = ResourceTypes.SCENE
@@ -34,3 +45,49 @@ class ScenesController(BaseResourcesController[Type[Scene]]):
         """Get group attached to given scene id."""
         scene = self[id]
         return next((x for x in self._bridge.groups if x.id == scene.group.rid))
+
+
+class SmartScenesController(BaseResourcesController[Type[SmartScene]]):
+    """Controller holding and managing HUE resources of type `smart_scene`."""
+
+    item_type = ResourceTypes.SMART_SCENE
+    item_cls = SmartScene
+    allow_parser_error = True
+
+    async def recall(
+        self, id: str, action: SmartSceneRecallAction = SmartSceneRecallAction.ACTIVATE
+    ) -> None:
+        """Turn on / recall scene."""
+        update_obj = SmartScenePut(recall=SmartSceneRecall(action=action))
+        await self.update(id, update_obj)
+
+    def get_group(self, id: str) -> Union[Room, Zone]:
+        """Get group attached to given scene id."""
+        scene = self[id]
+        return next((x for x in self._bridge.groups if x.id == scene.group.rid))
+
+
+class ScenesController(GroupedControllerBase[SCENE_TYPES]):
+    """Controller grouping resources of all sensor resources."""
+
+    def __init__(self, bridge: "HueBridgeV2") -> None:
+        """Initialize instance."""
+        self.scene = RegularScenesController(bridge)
+        self.smart_scene = SmartScenesController(bridge)
+        super().__init__(
+            bridge,
+            [
+                self.scene,
+                self.smart_scene,
+            ],
+        )
+
+    async def recall(self, id: str, *args: Any, **kwargs: Any) -> None:
+        """Turn on / recall scene."""
+        scene = self[id]
+        # forward call to correct controller
+        # this method is here for convenience (and backwards compatibility)
+        if isinstance(scene, SmartScene):
+            await self.smart_scene.recall(id, *args, **kwargs)
+            return
+        await self.scene.recall(id, *args, **kwargs)
