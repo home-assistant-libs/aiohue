fe59c7754470a3c935a6b73b9018bc7d5aa112fd
Ino Dekker <ino.dekker@gmail.com>
2024-08-24 14:15:53 +0200

    Test handling of events in base resource controller

--- tests/v2/test_base_resource_controller.py
+++ tests/v2/test_base_resource_controller.py
@@ -1,7 +1,122 @@
 """Test base controller functions."""
 
+from dataclasses import dataclass
+from unittest.mock import Mock
+from uuid import uuid4
+
 from aiohue import HueBridgeV2
+from aiohue.v2 import EventType
+from aiohue.v2.controllers.base import BaseResourcesController
 from aiohue.v2.controllers.sensors import ButtonController, RelativeRotaryController
+from aiohue.v2.models.resource import ResourceIdentifier, ResourceTypes
+
+
+@dataclass
+class MockData:
+    """Mock data resource type."""
+
+    id: str
+    type: ResourceTypes = ResourceTypes.UNKNOWN
+
+    owner: ResourceIdentifier | None = None
+    id_v1: str | None = None
+
+
+class MockController(BaseResourcesController[type[MockData]]):
+    """Controller for mock data resource."""
+
+    item_type = ResourceTypes.UNKNOWN
+    item_cls = MockData
+    allow_parser_error = False
+
+
+async def test_handle_event():
+    """Test handling of events."""
+    bridge = HueBridgeV2("127.0.0.1", "fake")
+    controller = MockController(bridge)
+    callback = Mock(return_value=None)
+    controller.subscribe(callback)
+
+    resource_id = str(uuid4())
+    device_id = str(uuid4())
+
+    evt_data = {
+        "id": resource_id,
+        "type": "unknown",
+        "id_v1": "mock/1",
+        "owner": {"rid": device_id, "rtype": "device"},
+    }
+
+    # Create a new resource
+    # pylint: disable=protected-access
+    await controller._handle_event(EventType.RESOURCE_ADDED, evt_data, False)
+
+    cur_data = MockData(
+        id=resource_id,
+        type=ResourceTypes.UNKNOWN,
+        id_v1="mock/1",
+        owner=ResourceIdentifier(rid=device_id, rtype=ResourceTypes.DEVICE),
+    )
+    callback.assert_called_with(EventType.RESOURCE_ADDED, cur_data)
+    callback.reset_mock()
+
+    evt_data = {
+        "id": resource_id,
+        "id_v1": "mock/2",
+    }
+
+    # Update of a single property of an existing resource
+    # pylint: disable=protected-access
+    await controller._handle_event(EventType.RESOURCE_UPDATED, evt_data, False)
+
+    cur_data = MockData(
+        id=resource_id,
+        type=ResourceTypes.UNKNOWN,
+        id_v1="mock/2",
+        owner=ResourceIdentifier(rid=device_id, rtype=ResourceTypes.DEVICE),
+    )
+    callback.assert_called_with(EventType.RESOURCE_UPDATED, cur_data)
+    callback.reset_mock()
+
+    evt_data = {
+        "id": device_id,
+        "id_v1": "mock/1",
+    }
+
+    # Update of a single property of a non-existing resource
+    # pylint: disable=protected-access
+    await controller._handle_event(EventType.RESOURCE_UPDATED, evt_data, False)
+
+    callback.assert_not_called()
+    callback.reset_mock()
+
+    evt_data = {
+        "id": resource_id,
+    }
+
+    # Remove of existing resource
+    # pylint: disable=protected-access
+    await controller._handle_event(EventType.RESOURCE_DELETED, evt_data, False)
+
+    cur_data = MockData(
+        id=resource_id,
+        type=ResourceTypes.UNKNOWN,
+        id_v1="mock/2",
+        owner=ResourceIdentifier(rid=device_id, rtype=ResourceTypes.DEVICE),
+    )
+    callback.assert_called_with(EventType.RESOURCE_DELETED, cur_data)
+    callback.reset_mock()
+
+    evt_data = {
+        "id": resource_id,
+        "id_v1": "mock/2",
+    }
+
+    # Update of an already removed resource
+    # pylint: disable=protected-access
+    await controller._handle_event(EventType.RESOURCE_UPDATED, evt_data, False)
+
+    callback.assert_not_called()
 
 
 def test_handle_last_event_backwards_compatibility_for_button():
